dcb544f7e1c1283a69236cfb7970f6146ef5e89d
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-02-27 18:38:26 -0600

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,6 @@
 int main()
 {
   std::cout << "Hello world" << std::endl;
-  std::cout << "This is a new feature! So super awesome!" << std::endl;
+  std::cout << "This is a new feature! This is some different text!" << std::endl;
   return 0;
 }
